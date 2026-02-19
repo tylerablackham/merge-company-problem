bc38c6bc03424224a227ecbeea5013bb5dd0ce47
Tyler Blackham <tylerablackham@gmail.com>
2026-02-19 14:10:39 -0700

    feat: setup fastify

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,3 @@
-console.log("Hello World!")
+import {startServer} from "./server.js";
+
+startServer().catch(console.error)
---
+++ backend/src/server.ts
@@ -0,0 +1,18 @@
+import Fastify from "fastify";
+
+const fastify = Fastify();
+
+// Define a route
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' };
+});
+
+export async function startServer() {
+  try {
+    await fastify.listen({ port: 3000, host: '0.0.0.0' });
+    console.log('Server running at http://localhost:3000');
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
